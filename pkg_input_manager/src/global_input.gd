d3463e4e23e6829595b07fa0439760220df027ba
Dan <newwby@gmail.com>
2022-10-10 09:44:47 +0100

    needs/requirements update

---
+++ pkg_input_manager/src/global_input.gd
@@ -0,0 +1,13 @@
+extends Input
+
+# This is designed as an autoload singleton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
