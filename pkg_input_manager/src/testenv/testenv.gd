8c388935508938bc8a18e4aca4b297a2f1e69145
Dan <newwby@gmail.com>
2022-10-11 17:08:57 +0100

    testing changes

--- pkg_input_manager/src/testenv/testenv.gd
+++ pkg_input_manager/src/testenv/testenv.gd
@@ -9,9 +9,17 @@ onready var GlobalInput = preload(GLOBAL_INPUT_PATH).new()
 func _ready():
 	pass # Replace with function body.
 	GlobalInput._export_project_input_map_to_disk()
+#	CompoundActionExtension.new()
+	var save_path = "res://pkg_input_manager/def/caex.tres"
+	var caex = CompoundActionExtension.new()
+	caex.test_var = 45
+	var result = ResourceSaver.save(save_path, caex)
+	print(save_path)
 #	print(ResourceSaver.get_recognized_extensions(Shader.new()))
+	assert(result == OK)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
