b36b990c57d2caa9e2170d0a845c2843d8b68ef3
Dan <newwby@gmail.com>
2022-10-11 03:14:48 +0100

    environment and editor updates

---
+++ pkg_input_manager/src/testenv/testenv.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+# placeholder_autoload_input
+const GLOBAL_INPUT_PATH = "res://pkg_input_manager/src/singleton/global_input.gd"
+onready var GlobalInput = preload(GLOBAL_INPUT_PATH).new()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	GlobalInput._export_project_input_map_to_disk()
+#	print(ResourceSaver.get_recognized_extensions(Shader.new()))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
