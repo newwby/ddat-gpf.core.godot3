026a1250f7b960898c5225ca65572623956fe065
Dan <60653155+newwby@users.noreply.github.com>
2023-01-16 07:29:34 +0000

    updated test scene
    now checks for missing test files and rewrites them if it can't find them
    manual tests under their own functions now

--- _testenv/ddat_core/ddat_core_data_sample_scene.gd
+++ _testenv/ddat_core/ddat_core_data_sample_scene.gd
@@ -1,10 +1,6 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+##############################################################################
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,10 +12,32 @@ func _ready():
 				"1": _unit_test_save_resource_path_to_user_data(),
 				"2": _unit_test_load_invalid_resource_path()
 				}))
-	pass
-	#
-	# custom manual testing
-	#
+	_global_data_test_resource()
+	_global_data_test_game_data_container()
+
+
+##############################################################################
+
+
+func test_save(player_save, datacon_dir: String, datacon_file: String):
+#	var player_save := GameDataContainer.new()
+	var _return_arg =\
+			GlobalData.save_resource(datacon_dir, datacon_file, player_save)
+
+
+func test_load(datacon_dir: String, datacon_file: String, type_cast = null):
+	var save_file = GlobalData.load_resource(
+			datacon_dir+datacon_file,
+			type_cast
+	)
+	return save_file
+
+
+##############################################################################
+
+
+# custom manual testing part 1
+func _global_data_test_resource():
 	var get_test_path = GlobalData.get_dirpath_user()
 #	var get_test_path = GlobalData.DATA_PATHS[GlobalData.DATA_PATH_PREFIXES.USER]
 	get_test_path += "test/test2/test3/test4/"
@@ -33,11 +51,25 @@ func _ready():
 #		var sample_path = GlobalData.get_dirpath_user()+"res.tres"
 		var sample_path = GlobalData.get_dirpath_user()+"resource_new.tres"
 #		var sample_path = GlobalData.get_dirpath_user()+"score.save"
-		var _new_res = GlobalData.load_resource(sample_path)
-	#
-	#
-#	test_save(GameDataContainer.new())
-	var get_save_res = test_load(GameDataContainer)
+		var _new_res
+		if GlobalData.validate_path(sample_path) == false:
+			_new_res = GlobalData.save_resource(
+				GlobalData.get_dirpath_user(),
+				"resource_new.tres",
+				Resource.new()
+			)
+		else:
+			_new_res = GlobalData.load_resource(sample_path)
+
+
+# custom manual testing part 2
+# save file 'gameDataContainer' testing
+func _global_data_test_game_data_container():
+	var datacon_dir: String = GlobalData.get_dirpath_user()+"saves/"
+	var datacon_file := "save1.tres"
+	if not GlobalData.validate_path(datacon_dir+datacon_file):
+		test_save(GameDataContainer.new(), datacon_dir, datacon_file)
+	var get_save_res = test_load(datacon_dir, datacon_file, GameDataContainer)
 	if get_save_res != null:
 		if "get_class" in get_save_res:
 			get_save_res.get_class()
@@ -54,25 +86,10 @@ func _ready():
 			}))
 			# now save it to file
 			get_save_res.example_float_data = get_float_data+increase
-			test_save(get_save_res)
+			test_save(get_save_res, datacon_dir, datacon_file)
 
 
-func test_save(player_save):
-	var datacon_dir: String = GlobalData.get_dirpath_user()+"saves/"
-	var datacon_file := "save1.tres"
-#	var player_save := GameDataContainer.new()
-	var _return_arg =\
-			GlobalData.save_resource(datacon_dir, datacon_file, player_save)
-
-
-func test_load(type_cast_test = null):
-	var datacon_dir: String = GlobalData.get_dirpath_user()+"saves/"
-	var datacon_file := "save1.tres"
-	var save_file = GlobalData.load_resource(
-			datacon_dir+datacon_file,
-			type_cast_test
-	)
-	return save_file
+##############################################################################
 
 
 # paths must begin with user://
@@ -104,59 +121,3 @@ func _unit_test_load_invalid_resource_path() -> bool:
 		_end_result = false
 	return _end_result
 
-
-#func old2():
-#	pass # Replace with function body.
-#	GlobalData.save_gdc()
-#	var new_res = GlobalData.load_gdc()
-#	print(new_res)
-#	if "get_class" in new_res:
-#		print("get_class"+" = ", new_res.get_class())
-#	else:
-#		print("get_class", " not found")
-#	if "get_property_list" in new_res:
-#		print("get_property_list"+" = ", new_res.get_property_list())
-#	else:
-#		print("get_property_list", " not found")
-#	if new_res is Resource:
-#		print("new res is resource")
-#	else:
-#		print("new res not resource")
-#	if new_res is GameDataContainer:
-#		print("new res is game data cotnainer")
-#	else:
-#		print("new res is not game data container")
-#
-#	if "example_int_data" in new_res:
-#		print("example_int_data"+" = ", new_res.example_int_data)
-#	if "example_float_data" in new_res:
-#		print("example_float_data"+" = ", new_res.example_float_data)
-#	if "example_bool_data" in new_res:
-#		print("example_bool_data"+" = ", new_res.example_bool_data)
-	
-	
-	
-
-#func old():
-#	GlobalData.save_to_file()
-#	var c = GlobalData.load_from_file()
-#	print(c.get_class())
-#	print(c.is_class("Node"))
-##	print(c.is_class("CustomObject"))
-#	print(c is CustomObject)
-#	if c is CustomObject:
-#		c.say_hello()
-#	else:
-#		print("inval obj")
-#	if c != null:
-#		if c.has_method("say_hello"):
-#			c.say_hello()
-#		if c.has_method("get_property_list"):
-#			for prop in c.get_property_list():
-#				print(prop)
-##			print(c.get_property_list())
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
