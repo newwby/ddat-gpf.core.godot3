1b0b07095bbfd701477c3a8e23fe5b815302e648
Dan <60653155+newwby@users.noreply.github.com>
2023-01-18 02:23:20 +0000

    unit test for get_file_paths

--- _testenv/ddat_core/ddat_core_data_sample_scene.gd
+++ _testenv/ddat_core/ddat_core_data_sample_scene.gd
@@ -6,14 +6,13 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# caution: running unit tests will push a lot of (intentional) errors
-	_run_unit_tests(false)
+	_run_unit_tests(true)
 	
 	# run manual tests
 	var run_manual_tests = false
 	if run_manual_tests:
 		_manualtest_datamgr_resource()
 		_manualtest_datamgr_game_data_container()
-		_manualtest_datamgr_get_paths()
 
 ##############################################################################
 
@@ -91,36 +90,36 @@ func _manualtest_datamgr_game_data_container():
 #// can extend this into a unit test by
 # - validating or creating testing directory and files at start
 # - turning below logic into a loop
-func _manualtest_datamgr_get_paths():
-	var path = GlobalData.DATA_PATHS[GlobalData.DATA_PATH_PREFIXES.GAME_SAVE]
-	
-	var get_files = GlobalData.get_file_paths(path)
-	print("test1", " expected return", get_files)
-	print("#\n")
-	
-	get_files = GlobalData.get_file_paths(path, "egg")
-	print("test2", " expected fail", get_files)
-	print("#\n")
-	
-	get_files = GlobalData.get_file_paths(path, "sav")
-	print("test3", " expected return", get_files)
-	print("#\n")
-	
-	get_files = GlobalData.get_file_paths(path, "", ".res")
-	print("test4", " expected fail", get_files)
-	print("#\n")
-	
-	get_files = GlobalData.get_file_paths(path, "", ".tres")
-	print("test5", " expected return", get_files)
-	print("#\n")
-	
-	get_files = GlobalData.get_file_paths(path, "", "", "save")
-	print("test6", " expected fail", get_files)
-	print("#\n")
-	
-	get_files = GlobalData.get_file_paths(path, "", "", "buttermilk")
-	print("test7", " expected return", get_files)
-	print("#")
+#func _manualtest_datamgr_get_paths():
+#	var path = GlobalData.DATA_PATHS[GlobalData.DATA_PATH_PREFIXES.GAME_SAVE]
+#
+#	var get_files = GlobalData.get_file_paths(path)
+#	print("test1", " expected return", get_files)
+#	print("#\n")
+#
+#	get_files = GlobalData.get_file_paths(path, "egg")
+#	print("test2", " expected fail", get_files)
+#	print("#\n")
+#
+#	get_files = GlobalData.get_file_paths(path, "sav")
+#	print("test3", " expected return", get_files)
+#	print("#\n")
+#
+#	get_files = GlobalData.get_file_paths(path, "", ".res")
+#	print("test4", " expected fail", get_files)
+#	print("#\n")
+#
+#	get_files = GlobalData.get_file_paths(path, "", ".tres")
+#	print("test5", " expected return", get_files)
+#	print("#\n")
+#
+#	get_files = GlobalData.get_file_paths(path, "", "", "save")
+#	print("test6", " expected fail", get_files)
+#	print("#\n")
+#
+#	get_files = GlobalData.get_file_paths(path, "", "", "buttermilk")
+#	print("test7", " expected return", get_files)
+#	print("#")
 
 
 
@@ -132,13 +131,17 @@ func _run_unit_tests(do_tests: bool = false):
 	var run_unit_tests = do_tests
 	print("run unit tests = ", run_unit_tests)
 	if run_unit_tests:
+		# temporarily removed path_to_user_data and resource_path
+		# as they push too many errors
 		var unit_test_record = {
-			"save_resource_path_to_user_data":
-				_unit_test_save_resource_path_to_user_data(),
-			"load_invalid_resource_path":
-				_unit_test_load_invalid_resource_path(),
+#			"save_resource_path_to_user_data":
+#				_unit_test_save_resource_path_to_user_data(),
+#			"load_invalid_resource_path":
+#				_unit_test_load_invalid_resource_path(),
 			"save_and_load_resource":
-				_unit_test_save_and_load_resource()
+				_unit_test_save_and_load_resource(),
+			"_unit_test_get_paths_main":
+				_unit_test_get_paths_main()
 		}
 		for test_id in unit_test_record:
 			print("running test {x}, result = {r}".format({
@@ -181,3 +184,156 @@ func _unit_test_load_invalid_resource_path() -> bool:
 #	save it to disk, then attempt to load it
 func _unit_test_save_and_load_resource():
 	pass
+
+
+# unit test for different inputs to the globalData.get_path method
+# this unit test will rewrite the directory/files each time
+func _unit_test_get_paths_main():
+	# start test by validating (and writing if messing) the test files
+	var test_save_path := "user://unit_test/get_paths/"
+	var test_file_1 := "file1.tres"
+	var test_file_2 := "file2.tres"
+	var test_file_3 := "file3.tres"
+	
+	# make sure this directory and these files exist
+	# create directory return error and breaks if directory found, so ignore
+	#//UPDATE directory creation removed as is handled by save_resource
+#	var _discard = GlobalData.create_directory(test_save_path)
+
+	# validating files is important
+	if GlobalData.save_resource(
+			test_save_path, test_file_1, Resource.new()) != OK:
+		print("test setup error 1")
+		return
+	if GlobalData.save_resource(
+			test_save_path, test_file_2, Resource.new()) != OK:
+		print("test setup error 2")
+		return
+	if GlobalData.save_resource(
+			test_save_path, test_file_3, Resource.new()) != OK:
+		print("test setup error 3")
+		return
+	
+	var expected_result_full: PoolStringArray = [
+		(test_save_path+test_file_1),
+		(test_save_path+test_file_2),
+		(test_save_path+test_file_3)
+	]
+	var expected_result_partial1: PoolStringArray = [
+		(test_save_path+test_file_1)
+	]
+#	var expected_result_partial2 := [
+#		(test_save_path+test_file_1),
+#		(test_save_path+test_file_3)
+#	]
+	var expected_result_empty: PoolStringArray = []
+	
+	
+	# run unit tests,
+	# compare expected result vs actual result as a bool
+	# then compare the bool against result so a single false fails the tests
+	var unit_result := true
+	var end_result := true
+	var expected_result: PoolStringArray = []
+	var get_file_paths_result: PoolStringArray = []
+	var test_id := 0
+	print("beginning tests for _unit_test_get_paths_main")
+	
+	# test get_file_paths works
+	get_file_paths_result = GlobalData.get_file_paths(test_save_path)
+	expected_result = expected_result_full
+	unit_result = (expected_result == get_file_paths_result)
+	end_result = (end_result and unit_result)
+	test_id += 1
+	#// GlobalDebug need a minor logging method
+	print("test no.{n} = {r}! \nexpected {e}, \noutcome {o}\n".format({
+		"n": test_id,
+		"r": unit_result,
+		"e": expected_result,
+		"o": get_file_paths_result
+	}))
+
+	# test for whether prefix req works (files should start w/'file')
+	get_file_paths_result = GlobalData.get_file_paths(test_save_path, "file")
+	expected_result = expected_result_full
+	unit_result = (expected_result == get_file_paths_result)
+	end_result = (end_result and unit_result)
+	test_id += 1
+	print("test no.{n} = {r}! \nexpected {e}, \noutcome {o}\n".format({
+		"n": test_id,
+		"r": unit_result,
+		"e": expected_result,
+		"o": get_file_paths_result
+	}))
+	
+	# test for whether prefix req works (files should not start w/'roar')
+	get_file_paths_result = GlobalData.get_file_paths(test_save_path, "roar")
+	expected_result = expected_result_empty
+	unit_result = (expected_result == get_file_paths_result)
+	end_result = (end_result and unit_result)
+	test_id += 1
+	print("test no.{n} = {r}! \nexpected {e}, \noutcome {o}\n".format({
+		"n": test_id,
+		"r": unit_result,
+		"e": expected_result,
+		"o": get_file_paths_result
+	}))
+	
+	# test for whether suffix req works (files should end in .tres)
+	get_file_paths_result = GlobalData.get_file_paths(test_save_path, "", ".tres")
+	expected_result = expected_result_full
+	unit_result = (expected_result == get_file_paths_result)
+	end_result = (end_result and unit_result)
+	test_id += 1
+	print("test no.{n} = {r}! \nexpected {e}, \noutcome {o}\n".format({
+		"n": test_id,
+		"r": unit_result,
+		"e": expected_result,
+		"o": get_file_paths_result
+	}))
+	
+	# test for whether suffix req works (files should not end in .save)
+	get_file_paths_result = GlobalData.get_file_paths(test_save_path, "", ".save")
+	expected_result = expected_result_empty
+	unit_result = (expected_result == get_file_paths_result)
+	end_result = (end_result and unit_result)
+	test_id += 1
+	print("test no.{n} = {r}! \nexpected {e}, \noutcome {o}\n".format({
+		"n": test_id,
+		"r": unit_result,
+		"e": expected_result,
+		"o": get_file_paths_result
+	}))
+	
+	# test for whether force exclude works (files should not include '2' or '3')
+	get_file_paths_result = GlobalData.get_file_paths(test_save_path, "", "", ["2", "3"])
+	expected_result = expected_result_partial1
+	unit_result = (expected_result == get_file_paths_result)
+	end_result = (end_result and unit_result)
+	test_id += 1
+	print("test no.{n} = {r}! \nexpected {e}, \noutcome {o}\n".format({
+		"n": test_id,
+		"r": unit_result,
+		"e": expected_result,
+		"o": get_file_paths_result
+	}))
+	
+	# test for whether force include works (files should include '1')			
+	get_file_paths_result = GlobalData.get_file_paths(test_save_path, "", "", [], ["1"])
+	expected_result = expected_result_partial1
+	unit_result = (expected_result == get_file_paths_result)
+	end_result = (end_result and unit_result)
+	test_id += 1
+	print("test no.{n} = {r}! \nexpected {e}, \noutcome {o}\n".format({
+		"n": test_id,
+		"r": unit_result,
+		"e": expected_result,
+		"o": get_file_paths_result
+	}))
+	
+#	print("_unit_test_get_paths_main, final outcome = ", end_result)
+	# end unit test
+	return end_result
+
+
+
