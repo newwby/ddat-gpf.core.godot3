1fbfd5f0865995c49ffce6d3127ad8db45aa8a0a
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2023-04-26 21:35:44 +0100

    update and move default style guide

---
+++ _dev/script_templates/style_guide.gd
@@ -0,0 +1,58 @@
+
+#01. tool
+#02. extends <- switched with class_name (originally 02.)
+#03. class_name <- switched with extends (originally 03.)
+
+##############################################################################
+
+# This is a slightly modified version of the 3.0 (rev 5f5a9378)  style guide
+# Changes are documented below with <- indicators
+# https://docs.godotengine.org/en/stable/tutorials/scripting/gdscript/gdscript_styleguide.html
+
+#04a. dependencies <- new addition
+#04b. # docstring
+#04c. #//TODO
+
+##############################################################################
+
+#05. signals
+#06. enums
+#07. constants
+#
+#08. exported variables
+#09. public variables
+#10. private variables
+#11. onready variables
+
+##############################################################################
+
+# virtual methods
+
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+#	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+##############################################################################
+
+# public methods
+
+
+#func example_method():
+#	pass
+
+
+##############################################################################
+
+# private methods
+
+
+#func example_method():
+#	pass
+
