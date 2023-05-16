3f2d07c7c433a3d2c596aa7a33ee42d7bdc9a762
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2023-05-16 15:59:28 +0100

    Update template_unit_test.gd

--- _dev/script_templates/template_unit_test.gd
+++ _dev/script_templates/template_unit_test.gd
@@ -14,12 +14,23 @@ extends UnitTest
 
 ##############################################################################
 
+
+func ready_test(property_register: Dictionary) -> void:
+	.ready_test(property_register)
+
+
 # public methods
 func start_test() -> bool:
+	if not is_test_readied:
+		GlobalLog.warning(self, "test could not start")
+		return false
 	var test_outcome := false
 	# console line break before log
 	print()
 	GlobalLog.trace(self, "test start")
+	
+	# put test logic here
+	
 	GlobalLog.trace(self, "final test outcome: {0}".format([test_outcome]))
 	is_test_readied = false
 	test_iteration_total += 1
