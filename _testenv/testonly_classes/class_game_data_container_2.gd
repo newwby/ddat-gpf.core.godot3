58eec2b107bb6a0a831527f548d880d8543c5097
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2023-06-16 10:37:56 +0100

    gamedatacontainer extensions for testing

---
+++ _testenv/testonly_classes/class_game_data_container_2.gd
@@ -0,0 +1,10 @@
+extends GameDataContainer
+
+class_name GameDataContainer2
+
+##############################################################################
+#
+# Sample extended class for testing inherited methods (originally for
+#	globalFunc.get_inheritance_from_name)
+#
+##############################################################################
---
+++ _testenv/testonly_classes/class_game_data_container_3.gd
@@ -0,0 +1,10 @@
+extends GameDataContainer2
+
+class_name GameDataContainer3
+
+##############################################################################
+#
+# Sample extended class for testing inherited methods (originally for
+#	globalFunc.get_inheritance_from_name)
+#
+##############################################################################
