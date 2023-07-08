3f6b7d615c98a8eb72e254f64b0427b751934a9a
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2023-07-09 00:37:19 +0100

    ActionMenuItem local class

--- src/ddat-gpf/core/ui/devtools/dev_action_menu.gd
+++ src/ddat-gpf/core/ui/devtools/dev_action_menu.gd
@@ -47,6 +47,83 @@ onready var margin_node = $Margin
 
 ##############################################################################
 
+# classes
+
+
+# data container
+class ActionMenuItem:
+	
+	var key := ""
+	var button_node_ref: Button = null setget _set_button_node_ref
+	var caller_node_ref: Node = null
+	var caller_method_name := ""
+	var is_valid := false
+	var in_tree := false
+	var waiting_to_join_tree := false
+	
+	func _init(
+			arg_key: String = "",
+			arg_button_node_ref: Button = null,
+			arg_caller_node_ref: Node = null,
+			arg_caller_method_name := ""):
+		if arg_key == null\
+		or arg_button_node_ref == null\
+		or arg_caller_node_ref == null\
+		or arg_caller_method_name == "":
+			is_valid = false
+		else:
+			self.key = arg_key
+			self.button_node_ref = arg_button_node_ref
+			self.caller_node_ref = arg_caller_node_ref
+			self.caller_method_name = arg_caller_method_name
+			is_valid = true
+	
+	
+	# when button is set, setup automatic behaviour for button pressing and
+	#	disabling call functionality if button exits the tree
+	func _set_button_node_ref(arg_value: Button):
+		button_node_ref = arg_value
+		if button_node_ref != null:
+			if button_node_ref.is_inside_tree() == false:
+				waiting_to_join_tree = true
+				yield(button_node_ref, "tree_entered")
+				waiting_to_join_tree = false
+			var signal_check_1 = GlobalFunc.confirm_connection(
+						button_node_ref, "pressed", self, "_on_button_pressed")
+			var signal_check_2 = GlobalFunc.confirm_connection(
+						button_node_ref, "tree_entered", self, "_on_button_enter_or_exit_tree")
+			var signal_check_3 = GlobalFunc.confirm_connection(
+						button_node_ref, "tree_exited", self, "_on_button_enter_or_exit_tree")
+			# check signals connected
+			if (signal_check_1+signal_check_2+signal_check_3) != OK:
+				GlobalLog.error(self, "button {0} setup invalid, ERR {1}-{2}-{3}".format([
+						button_node_ref, signal_check_1, signal_check_2, signal_check_3]))
+			in_tree = button_node_ref.is_inside_tree()
+	
+	
+	func _on_button_enter_or_exit_tree():
+		in_tree = button_node_ref.is_inside_tree()
+	
+	
+	# returns whether the call was successful (whether the method does
+	#	anything or not)
+	# method 'call_dev_action'
+	func _on_button_pressed() -> int:
+		if is_valid == false:
+			return ERR_UNCONFIGURED
+		if caller_node_ref == null:
+			return ERR_INVALID_PARAMETER
+		if caller_node_ref.is_inside_tree() == false:
+			return ERR_DOES_NOT_EXIST
+		if caller_node_ref.has_method(caller_method_name) == false:
+			return ERR_METHOD_NOT_FOUND
+		else:
+			caller_node_ref.caller_method_name()
+			return OK
+
+
+##############################################################################
+
 
 func _ready():
 	_setup_viewport_responsiveness()
