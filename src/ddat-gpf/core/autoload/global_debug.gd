2ca0ae8b3efce17488a08908ecff5a5d75e3dde3
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2023-07-09 15:12:57 +0100

    devActionMenu connection

--- src/ddat-gpf/core/autoload/global_debug.gd
+++ src/ddat-gpf/core/autoload/global_debug.gd
@@ -17,6 +17,7 @@ extends GameGlobal
 # visible ingame. This is useful to get feedback in unstable release builds.
 signal update_debug_overlay_item(item_key, item_value, item_position)
 
+signal add_dev_command(key, caller, method, add_button_command, add_text_command)
 
 # where on-screen to position a debug_overlay_item
 # passed alongside update_debug_overlay_item signal
@@ -36,11 +37,23 @@ func _ready():
 ###############################################################################
 
 
+# passes to DevActionMenu
+func add_dev_command(
+		arg_key: String,
+		arg_caller: Object,
+		arg_caller_method: String,
+		arg_add_menu_button: bool = true,
+		arg_add_console_command: bool = true):
+	emit_signal("add_dev_command",
+			arg_key, arg_caller, arg_caller_method,
+			arg_add_menu_button, arg_add_console_command)
+
+
 # updates a value on the debug overlay or creates a new key/value pair if
 #	the given overlay_item_key does not exist
 # arg_overlay_position sets where on-screen to show the overlay item;
 #	if it already exists it will be moved to there
-#enum 
+# passes to DevDebugOverlay
 func update_debug_overlay(
 		arg_overlay_item_key: String,
 		arg_overlay_item_value,
